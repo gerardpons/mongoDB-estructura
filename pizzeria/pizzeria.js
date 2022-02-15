801c3db4243ef0fbd582ed4f6d2e5fa81539bfb6
Gerard Pons <gpguisado@gmail.com>
2022-02-15 10:57:55 +0100

    reentrega pizzeria

--- pizzeria/pizzeria.js
+++ pizzeria/pizzeria.js
@@ -1,275 +1,236 @@
+use PIZZERIA
+
 db.createCollection('cliente', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        title: 'cliente',
-        required: ['nombre', 'apellido1', 'apellido2', 'direccion', 'codigo_postal', 'localidad', 'provincia', 'telefono'],
-        properties: {
-          nombre: {
-            bsonType: 'string'
-          },
-          apellido1: {
-            bsonType: 'string'
-          },
-          apellido2: {
-            bsonType: 'string'
-          },
-          direccion: {
-            bsonType: 'string'
-          },
-          codigo_postal: {
-            bsonType: 'string'
-          },
-          localidad: {
-            bsonType: 'objectId'
-          },
-          provincia: {
-            bsonType: 'objectId'
-          },
-          telefono: {
-            bsonType: 'string'
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      title: 'cliente',
+      properties: {
+        nombre: {
+          bsonType: 'string'
+        },
+        apellido1: {
+          bsonType: 'string'
+        },
+        apellido2: {
+          bsonType: 'string'
+        },
+        direccion: {
+          bsonType: 'object',
+          title: 'direccion',
+          required: ['localidad'],
+          properties: {
+            calle: {
+              bsonType: 'string'
+            },
+            numero: {
+              bsonType: 'string'
+            },
+            piso: {
+              bsonType: 'string'
+            },
+            puerta: {
+              bsonType: 'string'
+            },
+            codigo_postal: {
+              bsonType: 'string'
+            },
+            localidad: {
+              bsonType: 'objectId'
+            }
           }
+        },
+        telefono: {
+          bsonType: 'string'
         }
       }
     }
-  });
-
-  db.createCollection('localidad', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        title: 'localidad',
-        required: ['nombre', 'provincia'],
-        properties: {
-          nombre: {
-            bsonType: 'string'
-          },
-          provincia: {
-            bsonType: 'objectId'
-          }
+  }
+});
+db.createCollection('localidad', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      title: 'localidad',
+      required: ['provincia'],
+      properties: {
+        nombre: {
+          bsonType: 'string'
+        },
+        provincia: {
+          bsonType: 'objectId'
         }
       }
     }
-  });
-
-  db.createCollection('provincia', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        title: 'provincia',
-        required: ['nombre'],
-        properties: {
-          nombre: {
-            bsonType: 'string'
-          }
+  }
+});
+db.createCollection('provincia', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      title: 'provincia',
+      properties: {
+        nombre: {
+          bsonType: 'string'
         }
       }
     }
-  });
-
-  db.createCollection('pedido', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        title: 'pedido',
-        required: ['cliente', 'fecha_hora', 'tipo', 'precio', 'tienda'],
-        properties: {
-          cliente: {
-            bsonType: 'objectId'
-          },
-          fecha_hora: {
-            bsonType: 'date'
-          },
-          tipo: {
-            enum:
-          },
-          precio: {
-            bsonType: 'double'
-          },
-          tienda: {
-            bsonType: 'objectId'
-          },
-          hamburguesa: {
-            bsonType: 'objectId'
-          },
-          bebida: {
-            bsonType: 'objectId'
-          },
-          pizza: {
-            bsonType: 'objectId'
+  }
+});
+db.createCollection('tienda', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      title: 'tienda',
+      required: ['empleado'],
+      properties: {
+        direccion: {
+          bsonType: 'object',
+          title: 'direccion',
+          required: ['localidad'],
+          properties: {
+            calle: {
+              bsonType: 'string'
+            },
+            numero: {
+              bsonType: 'string'
+            },
+            piso: {
+              bsonType: 'string'
+            },
+            puerta: {
+              bsonType: 'string'
+            },
+            codigo_postal: {
+              bsonType: 'string'
+            },
+            localidad: {
+              bsonType: 'objectId'
+            }
           }
-        }
-      }
-    }
-  });
-
-  db.createCollection('pizza', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        title: 'pizza',
-        required: ['nombre', 'descripcion', 'imagen', 'precio', 'categoria'],
-        properties: {
-          nombre: {
-            bsonType: 'string'
-          },
-          descripcion: {
-            bsonType: 'string'
-          },
-          imagen: {
-            bsonType: 'binData'
-          },
-          precio: {
-            bsonType: 'double'
-          },
-          categoria: {
+        },
+        empleado: {
+          bsonType: 'array',
+          items: {
             bsonType: 'objectId'
           }
         }
       }
     }
-  });
-
-  db.createCollection('categoria', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        title: 'categoria',
-        required: ['nombre'],
-        properties: {
-          nombre: {
-            bsonType: 'string'
-          }
+  }
+});
+db.createCollection('empleado', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      title: 'empleado',
+      properties: {
+        nombre: {
+          bsonType: 'string'
+        },
+        apellido1: {
+          bsonType: 'string'
+        },
+        apellido2: {
+          bsonType: 'string'
+        },
+        nif: {
+          bsonType: 'string'
+        },
+        telefono: {
+          bsonType: 'string'
+        },
+        cargo: {
+          bsonType: 'string'
         }
       }
     }
-  });
-
-  db.createCollection('hamburguesa', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        title: 'hamburguesa',
-        required: ['nombre', 'descripcion', 'imagen', 'precio'],
-        properties: {
-          nombre: {
-            bsonType: 'string'
-          },
-          descripcion: {
-            bsonType: 'string'
-          },
-          imagen: {
-            bsonType: 'binData'
-          },
-          precio: {
-            bsonType: 'double'
+  }
+});
+db.createCollection('pedido', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      title: 'pedido',
+      required: ['cliente', 'tienda', 'producto'],
+      properties: {
+        fecha_hora: {
+          bsonType: 'date'
+        },
+        tipo: {
+          enum: ('domicilio', 'recoger')
+        },
+        precio: {
+          bsonType: 'double'
+        },
+        reparto_domicilio: {
+          bsonType: 'object',
+          title: 'reparto_domicilio',
+          required: ['repartidor'],
+          properties: {
+            hora_entrega: {
+              bsonType: 'date'
+            },
+            repartidor: {
+              bsonType: 'objectId'
+            }
           }
-        }
-      }
-    }
-  });
-
-  db.createCollection('bebida', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        title: 'bebida',
-        required: ['nombre', 'descripcion', 'imagen', 'precio'],
-        properties: {
-          nombre: {
-            bsonType: 'string'
-          },
-          descripcion: {
-            bsonType: 'string'
-          },
-          imagen: {
-            bsonType: 'binData'
-          },
-          precio: {
-            bsonType: 'double'
-          }
-        }
-      }
-    }
-  });
-
-  db.createCollection('tienda', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        title: 'tienda',
-        required: ['localidad', 'provincia'],
-        properties: {
-          direccion: {
-            bsonType: 'string'
-          },
-          codigo_postal: {
-            bsonType: 'string'
-          },
-          localidad: {
-            bsonType: 'objectId'
-          },
-          provincia: {
+        },
+        cliente: {
+          bsonType: 'objectId'
+        },
+        tienda: {
+          bsonType: 'objectId'
+        },
+        producto: {
+          bsonType: 'array',
+          items: {
             bsonType: 'objectId'
           }
         }
       }
     }
-  });
-
-  db.createCollection('empleado', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        title: 'empleado',
-        required: ['tienda', 'nombre', 'apellido1', 'apellido2', 'nif', 'telefono', 'cargo'],
-        properties: {
-          tienda: {
-            bsonType: 'objectId'
-          },
-          nombre: {
-            bsonType: 'string'
-          },
-          apellido1: {
-            bsonType: 'string'
-          },
-          apellido2: {
-            bsonType: 'string'
-          },
-          nif: {
-            bsonType: 'string'
-          },
-          telefono: {
-            bsonType: 'string'
-          },
-          cargo: {
-            enum:
-          },
-          pedido_entregado: {
-            bsonType: 'string'
-          }
+  }
+});
+db.createCollection('producto', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      title: 'producto',
+      required: ['categoria'],
+      properties: {
+        nombre: {
+          bsonType: 'string'
+        },
+        descripcion: {
+          bsonType: 'string'
+        },
+        imagen: {
+          bsonType: 'binData'
+        },
+        precio: {
+          bsonType: 'double'
+        },
+        tipo: {
+          enum: ('pizza', 'hamburguesa', 'bebida')
+        },
+        categoria: {
+          bsonType: 'objectId'
         }
       }
     }
-  });
-
-  db.createCollection('pedido_entregado', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        title: 'pedido_entregado',
-        required: ['fecha_hora', 'empleado', 'pedido'],
-        properties: {
-          fecha_hora: {
-            bsonType: 'date'
-          },
-          empleado: {
-            bsonType: 'objectId'
-          },
-          pedido: {
-            bsonType: 'objectId'
-          }
+  }
+});
+db.createCollection('categoria', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      title: 'categoria',
+      properties: {
+        nombre: {
+          bsonType: 'string'
         }
       }
     }
-  });
+  }
+});
